fa7b9816bdb2b06035794dc712a5b56f817acb8a
Alejandro Perdomo <diegoleandro-29@hotmail.com>
2023-05-21 18:39:48 -0500

    .gitignore and basic express configuration

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('hola mundo1');
+import express from 'express';
+
+//Create express server
+const app = express();
+
+//Routes
+app.get('/', (req, res) => {
+    console.log('ser requiere el /');
+    res.json({
+        ok: true
+    })
+});
+
+//Listen petition
+app.listen( 4000, () => {
+    console.log(`Running server on ${4000}`);
+})
