8347ba911d7dcb91e2645c9f725ec13e8ba58afa
Alejandro Perdomo <diegoleandro-29@hotmail.com>
2023-05-22 12:39:45 -0500

    Update event

--- controllers/events.js
+++ controllers/events.js
@@ -38,12 +38,51 @@ const createEvent = async(req, res = response) => {
     
 }
 
-const updateEvent = (req, res = response) => {
-    
-    res.json({
-        ok: true,
-        msg: 'updateEvent'
-    });
+const updateEvent = async(req, res = response) => {
+
+    const { id } = req.params;
+    const { uid } = req;
+
+    try {
+
+        const event = await Event.findById(id);
+        
+
+        if (!event) {
+            res.status(404).json({
+                ok: false,
+                msg: 'El evento no existe con ese id'
+            });
+        }
+
+        console.log(event.user.toString())
+
+        if (event.user.toString() !== uid) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene permisos para editar este evento'
+            });
+        }
+
+        const newEvent = {
+            ...req.body,
+            user: uid
+        }
+
+        const updateEvent = await Event.findByIdAndUpdate(id, newEvent, {new: true});
+
+        res.json({
+            ok: true,
+            event: updateEvent
+        });
+        
+    } catch (error) {
+        console.log('error', error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Contacte al admin'
+        })
+    }
 
 }
 
